573aa3ffff8e19290a8067bc6ea5835403d738d8
BrosLunas <pablo.luna.perez.008@gmail.com>
2024-06-27 15:43:36 +0200

    update config

--- astro.config.ts
+++ astro.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   // used to generate images
   site:
     process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
+      ? 'https://broslunas.vercel.app/'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+      : 'https://localhost:4321/',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
